d6587dffd7c11fc02e84ece34817e109083ba80d
XardoX <jurek.gab.jg@gmail.com>
2024-01-04 18:16:39 +0100

    loading level areas with addressables

---
+++ Assets/Scripts/AreaLoader.cs
@@ -0,0 +1,56 @@
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AI;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AreaLoader : MonoBehaviour
+{
+    [SerializeField]
+    private AssetReferenceGameObject area;
+
+    private GameObject instancedArea;
+
+    private NavMeshSurface surface;
+
+    private void Awake()
+    {
+        surface = GetComponentInParent<NavMeshSurface>();
+    }
+
+    private void LoadArea()
+    {
+        area.InstantiateAsync().Completed += OnAddressableLoaded;
+    }
+
+    private void UnloadArea()
+    {
+        area.ReleaseInstance(instancedArea);
+    }
+
+    private void OnAddressableLoaded(AsyncOperationHandle<GameObject> operation)
+    {
+        if (operation.Status == AsyncOperationStatus.Succeeded)
+        {
+            instancedArea = operation.Result;
+            instancedArea.transform.position = transform.position;
+            surface.BuildNavMesh();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("MainCamera"))
+        {
+            LoadArea();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            UnloadArea();
+        }
+    }
+}
